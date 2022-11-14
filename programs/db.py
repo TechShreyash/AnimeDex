3be3649f355cc7582b7f375077f30771df44f2e6
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-11-14 07:08:52 +0530

    Update db.py

--- programs/db.py
+++ programs/db.py
@@ -4,7 +4,15 @@ import requests
 def update_views(anime):
     try:
         requests.get(
-            'https://animedex-api.vercel.app/db/save?anime='+anime.strip())
+            'https://animedex-api.vercel.app/db/view?anime='+anime.strip())
+    except:
+        pass
+    return
+
+def update_watch(anime):
+    try:
+        requests.get(
+            'https://animedex-api.vercel.app/db/watch?anime='+anime.strip())
     except:
         pass
     return
