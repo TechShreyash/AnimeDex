657f494b88dff4091a39d725e90c853ba18645b7
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-12-10 21:41:17 +0530

    Deployed DB Api To VPS

--- programs/db.py
+++ programs/db.py
@@ -4,7 +4,7 @@ import requests
 def update_views(anime):
     try:
         requests.get(
-            'https://animedex-api.vercel.app/db/view?anime='+anime.strip())
+            'https://animedex-api.azurewebsites.net/db/view?anime='+anime.strip())
     except:
         pass
     return
@@ -13,7 +13,7 @@ def update_views(anime):
 def update_watch(anime):
     try:
         requests.get(
-            'https://animedex-api.vercel.app/db/watch?anime='+anime.strip())
+            'https://animedex-api.azurewebsites.net/db/watch?anime='+anime.strip())
     except:
         pass
     return
