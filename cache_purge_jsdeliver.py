8cc29c924729ae04bc4be6659d1515d6ad6c508b
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-12-22 09:06:39 +0530

    Jsdeliver Cache Purge Script

---
+++ cache_purge_jsdeliver.py
@@ -0,0 +1,16 @@
+import requests
+
+urls = ['https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/js/home.min.js',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/css/anime.min.css',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/css/episode.min.css',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/css/home.min.css',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/css/search.min.css',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/css/video.min.css',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/js/home.min.js',
+        'https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/js/player.min.js']
+
+
+for url in urls:
+    url = url[24:]
+    r = requests.get('https://purge.jsdelivr.net'+url)
+    print(r.json().get('status'), url)
