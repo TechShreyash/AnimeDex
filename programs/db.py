95ba221d4023a7855b27d0a87fa69c04ffbe1d56
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-04-25 22:07:53 +0530

    Improved Stats System

--- programs/db.py
+++ programs/db.py
@@ -1,17 +1,22 @@
 import requests
+from config import API_KEY
 
 
 def update_views(anime):
     try:
-        requests.get("https://api.animedex.live/db/view?anime=" + anime.strip())
-    except:
-        pass
+        requests.get(
+            f"https://api.animedex.live/db/view?key={API_KEY}&anime={anime.strip()}"
+        )
+    except Exception as e:
+        print(e)
     return
 
 
 def update_watch(anime):
     try:
-        requests.get("https://api.animedex.live/db/watch?anime=" + anime.strip())
-    except:
-        pass
+        requests.get(
+            f"https://api.animedex.live/db/watch?key={API_KEY}&anime={anime.strip()}"
+        )
+    except Exception as e:
+        print(e)
     return
